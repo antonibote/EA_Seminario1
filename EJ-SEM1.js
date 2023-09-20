4f748aa17aba57ebe938d073903eebcc3d0d75ac
antonibote <antoni.bote@estudiantat.upc.edu>
2023-09-20 15:29:35 +0200

    Error Management added

--- EJ-SEM1.js
+++ EJ-SEM1.js
@@ -1,10 +1,17 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  
+
+  .then( response => {
+    if (!response.ok)
+      throw new Error(`HTTP Error (${response.status})`)
+    return response.json() 
+  })
+
   .then((json) => json
   .filter((song) => song.id > 50)
   .map((song) => song.id)
   .reduce((a, b) => a + b, 0))
-
+  
+  .catch( err => console.log(`Error: ${err}`))
   .then((json) => console.log(json));
 
+  //Repositorio github: git@github.com:antonibote/EA_Seminario1.git
